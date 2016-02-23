bb71c292865902b62806b6745c8eac4ffb253eac
Zachary Green <zachjamesgreen@gmail.com>
2016-02-22 23:25:30 -0700

    epic editor is giving me problems

--- app/assets/javascripts/railway/application.js
+++ app/assets/javascripts/railway/application.js
@@ -10,6 +10,7 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require epiceditor
 //= require moment
 //= require moment/en-gb
 //= require godmin
