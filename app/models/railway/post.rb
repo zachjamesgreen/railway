76f907e0171c8a9671eb81f2d91c5f1afc7ad7df
Zachary Green <zachjamesgreen@gmail.com>
2016-03-01 19:03:09 -0700

    add validation to posts

--- app/models/railway/post.rb
+++ app/models/railway/post.rb
@@ -1,5 +1,7 @@
 module Railway
   class Post < ActiveRecord::Base
+    validates :title, :author, :content, :slug, presence: true
+    validates :title, :slug, uniqueness: { case_sensitive: false }
 
     def to_s
       self.title
